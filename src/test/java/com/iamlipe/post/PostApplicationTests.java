cb5fbbd3e61bb9a394dbf19ef9857aa6f4e1d38e
Felipe Lima <felipe_lima10@icloud.com>
2023-09-27 20:05:51 -0300

    init

---
+++ src/test/java/com/iamlipe/post/PostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iamlipe.post;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
