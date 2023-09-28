78f80fe2ab8bc7b53a912df83c182400dbfb7219
Felipe Lima <felipe_lima10@icloud.com>
2023-09-27 21:34:31 -0300

    Obtendo um usuário por id

--- src/main/java/com/iamlipe/post/resources/UserResource.java
+++ src/main/java/com/iamlipe/post/resources/UserResource.java
@@ -5,6 +5,7 @@ import com.iamlipe.post.dto.UserDTO;
 import com.iamlipe.post.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,5 +25,12 @@ public class UserResource {
         List<User> users = service.findAll();
         List<UserDTO> userDTOS = users.stream().map(UserDTO::new).collect(Collectors.toList());
         return ResponseEntity.ok().body(userDTOS);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+        User user = service.findById(id);
+        UserDTO userDTO = new UserDTO(user);
+        return ResponseEntity.ok().body(userDTO);
     }
 }
--- src/main/java/com/iamlipe/post/services/UserService.java
+++ src/main/java/com/iamlipe/post/services/UserService.java
@@ -2,10 +2,12 @@ package com.iamlipe.post.services;
 
 import com.iamlipe.post.domain.User;
 import com.iamlipe.post.repositories.UserRepository;
+import com.iamlipe.post.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +17,10 @@ public class UserService {
 
     public List<User> findAll() {
         return repository.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("User not found"));
     }
 }
